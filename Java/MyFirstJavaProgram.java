756ef2f223e32a1a7d9c2d85ba4a8bcb98f4604c
Aerialith <xg625340883@gmail.com>
2022-05-28 21:15:01 +0800

    上传点java

---
+++ Java/MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        Date date = new Date();
+        //打印时间
+        System.out.println(date.toString());
+
+    }
+
+}
